d02c46e3b5061b0c574e05ca361baadbbd0b91a3
furusawakj176 <je_10042000@yahoo.co.jp>
2023-01-23 15:37:50 +0900

    Section2 Lesson3-4 完了

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -9,8 +9,40 @@ contract TokenFarm{
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    // 7. これまでにステーキングを行ったすべてのアドレスを追跡する配列を作成
+    address[] public stakers;
+
+    // 4. 投資家のアドレスと彼らのステーキングしたトークンの量を紐づけるmappingを作成
+    mapping (address => uint) public stakingBalance;
+
+    // 6. 投資家のアドレスをもとに彼らがステーキングを行ったか否かを紐づけるmappingを作成
+    mapping (address => bool) public hasStaked;
+
+    // 10. 投資家の最新のステイタスを記録するマッピングを作成
+    mapping (address => bool) public isStaking;
+
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
+
+  // 1.ステーキング機能を作成する
+  function stakeTokens(uint _amount) public {
+    // 2. ステーキングされるトークンが0以上あることを確認
+    require(_amount > 0, "amount can't be 0");
+    // 3. 投資家のトークンをTokenFarm.solに移動させる
+    daiToken.transferFrom(msg.sender, address(this), _amount);
+
+    // 5. ステーキングされたトークンの残高を更新する
+    stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+    // 8. 投資家がまだステークしていない場合のみ、かれらをstakers配列に追加する
+    if(!hasStaked[msg.sender]){
+        stakers.push(msg.sender);
+    }
+
+    // 9. ステーキングステータスの更新
+    isStaking[msg.sender] = true;
+    hasStaked[msg.sender] = true;
+  }
 }
